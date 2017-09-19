f46d792a399f98d2a3e367d2c0e619cc59f9c9f2
Felicia <felicias.sjogren@gmail.com>
2017-09-19 15:15:31 +0200

    Add files via upload

---
+++ ProjektHT17/scriptet.js
@@ -0,0 +1,27 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+} 
+
+function initMap() {
+        var location = {lat: 59.254276, lng: 15.24923};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: location
+
+        });
+
+       var marker = new google.maps.Marker({
+       	position: location,
+       	map: map
+       });
+       
+      }
+
+
+     
+
